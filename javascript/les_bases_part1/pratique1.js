186dd80595ac8481a46aa25ee0eed11e8f9623af
TheValll <valentin78.massonniere@gmail.com>
2023-09-25 14:50:36 +0200

    pratique1

---
+++ javascript/les_bases_part1/pratique1.js
@@ -0,0 +1,8 @@
+let Country = "France";
+let Population = 68;
+let Continent = "Europe";
+console.group("MesInformations");
+console.log(Country);
+console.log(Population);
+console.log(Continent);
+console.groupEnd();
---
+++ javascript/les_bases_part1/script.js
@@ -0,0 +1,13 @@
+// variables et values
+let num = 10;
+let string = "Hello";
+// console.log(string, num);
+num = 19;
+// console.log(num);
+num = "19";
+// console.log(num);
+// console.group();
+// console.log(num);
+// console.log(string);
+// console.groupEndg();
+// console.table(num, string);
