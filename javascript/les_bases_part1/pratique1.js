372e1d37bc7a2e746060500132a7c163f0620976
TheValll <valentin78.massonniere@gmail.com>
2023-09-27 19:15:44 +0200

    day3bis

--- javascript/les_bases_part1/pratique1.js
+++ javascript/les_bases_part1/pratique1.js
@@ -1,6 +1,9 @@
+// Declaration des variables
 let country = "France";
 let population = 68;
 let continent = "Europe";
+
+// Console log des variables
 console.group("MesInformations");
 console.log(country);
 console.log(population);
