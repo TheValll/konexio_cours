d640d6f199af3c825d4aeabd0094f3be00d5ce28
TheValll <valentin78.massonniere@gmail.com>
2023-09-28 10:56:04 +0200

    guess my number setup

---
+++ javascript/dom_events/script.js
@@ -0,0 +1,15 @@
+// console.log(document.querySelector(".message").textContent);
+// document.querySelector(".message").textContent = "bonjour";
+// console.log(document.querySelector(".message").textContent);
+
+// console.log(document.querySelector(".number").textContent);
+// document.querySelector(".number").textContent = 13;
+// console.log(document.querySelector(".number").textContent);
+
+// console.log(document.querySelector(".score").textContent);
+// document.querySelector(".score").textContent = 10;
+// console.log(document.querySelector(".score").textContent);
+
+// console.log(document.querySelector(".guess").value);
+// document.querySelector(".guess").value = 23;
+// console.log(document.querySelector(".guess").value);
