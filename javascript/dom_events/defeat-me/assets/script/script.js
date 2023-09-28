dc9db4d5ec5d53800d9e800d1f1b8a235ee173b0
TheValll <valentin78.massonniere@gmail.com>
2023-09-28 17:00:13 +0200

    defeat me css

---
+++ javascript/dom_events/defeat-me/assets/script/script.js
@@ -0,0 +1,32 @@
+const againInput = document.querySelector(".again");
+const discussionInput = document.querySelector(".discussion");
+const resultInput = document.querySelector("#result");
+const pendingInput = document.querySelector("#pendingBot-logo");
+const stoneBotInput = document.querySelector("#stoneBot-logo");
+const paperBotInput = document.querySelector("#paperBot-logo");
+const scissorsBotInput = document.querySelector("#scissorsBot-logo");
+const scoreInput = document.querySelector(".score");
+const lifeInput = document.querySelector(".life");
+const highscoreInput = document.querySelector(".highscore");
+const stoneInput = document.querySelector("#stone-logo");
+const paperInput = document.querySelector("#paper-logo");
+const scissorsInput = document.querySelector("#scissors-logo");
+const playInput = document.querySelector(".play");
+
+// let secretSolution;
+
+// let randomSolution = () => {
+//   let random = Math.floor(Math.random() * 3 + 1); // choix d'un chiffre aléatoire entre 1 et 3
+//   return random;
+// };
+// let random = randomSolution();
+// if (random === 1) {
+//   secretSolution = stoneBotInput;
+//   console.log("pierre");
+// } else if (random === 2) {
+//   secretSolution = paperBotInput;
+//   console.log("feuille");
+// } else if (random) {
+//   secretSolution = scissorsBotInput;
+//   console.log("ciseux");
+// }
