1c57bad95677315081af3d83ee5c1a6848fc5aa7
TheValll <valentin78.massonniere@gmail.com>
2023-09-28 17:05:44 +0200

    test2

---
+++ javascript/dom_events/guess-my-number-/script.js
@@ -0,0 +1,59 @@
+'use strict';
+
+let messageInput = document.querySelector('.message');
+let numberInput = document.querySelector('.number');
+let scoreInput = document.querySelector('.score');
+let checkInput = document.querySelector('.check');
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let body = document.querySelector('body');
+let againInput = document.querySelector('.again');
+let highscore = 0;
+let highscoreInput = document.querySelector('.highscore');
+const message = messages => {
+  return (document.querySelector('.message').textContent = messages);
+};
+// console.log(typeof numberInput);
+// console.log(typeof guess);
+console.log(secretNumber);
+
+// Checking if the input is valid
+
+checkInput.addEventListener('click', () => {
+  let guess = Number(document.querySelector('.guess').value);
+  if (!guess) {
+    messageInput.textContent = 'Not a number !';
+    numberInput.value = secretNumber;
+    // If Player win
+  } else if (guess === secretNumber) {
+    messageInput.textContent = 'You won the game';
+    body.style.backgroundColor = 'green';
+    numberInput.style.width = '30rem';
+    numberInput.textContent = secretNumber;
+    if (score > highscore) {
+      highscore = score;
+      highscoreInput.textContent = score;
+    }
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      message(guess > secretNumber ? 'Too High !' : 'Too Low !');
+      score--;
+      scoreInput.textContent = score;
+    } else {
+      message('Game Over !');
+      scoreInput.textContent = 0;
+    }
+  }
+});
+
+againInput.addEventListener('click', () => {
+  score = 20;
+  scoreInput.textContent = score;
+  messageInput.textContent = 'Start guessing ...';
+  body.style.backgroundColor = '#222';
+  numberInput.style.width = '15rem';
+  numberInput.textContent = '?';
+  let guess = Number((document.querySelector('.guess').value = ''));
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  console.log(secretNumber);
+});
