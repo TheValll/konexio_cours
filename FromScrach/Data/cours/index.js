6e6f5511eedce7a6595b98201d31eabab8f50aea
TheValll <valentin78.massonniere@gmail.com>
2023-09-29 17:24:16 +0200

    data

---
+++ FromScrach/Data/cours/index.js
@@ -0,0 +1,12 @@
+//// Les structures de controle
+
+/// If, else, while, do while, for, switch
+
+let d = 0;
+do {
+  d++;
+  //   console.log(d);
+} while (d < 1);
+
+/// e.target.id
+// setInterval(createLetter, 200);
