1c23e9dec989136431de4f9215f29f890dfc9c9b
TheValll <valentin78.massonniere@gmail.com>
2023-10-01 20:19:06 +0200

    memory game random numbers function

--- javascript/dom_events/memory-game/assets/javascript/script.js
+++ javascript/dom_events/memory-game/assets/javascript/script.js
@@ -8,22 +8,23 @@ const test = [];
 const test2 = [];
 
 // GIVE RANDOM NUMBERS AT CARD
-for (let i = 0; i < cardInput.length; i++) {
-  cardInput[i].addEventListener("click", () => {
-    const numbers = [];
-    while (numbers.length < 1) {
-      const randomNumber = Math.floor(Math.random() * 8) + 1;
-      if (!test.includes(randomNumber)) {
-        test.push(randomNumber);
-        numbers.push(randomNumber);
-      } else if (!test2.includes(randomNumber)) {
-        test2.push(randomNumber);
-        numbers.push(randomNumber);
+const random = () => {
+  for (let i = 0; i < cardInput.length; i++) {
+    cardInput[i].addEventListener("click", () => {
+      const numbers = [];
+      while (numbers.length < 1) {
+        const randomNumber = Math.floor(Math.random() * 8) + 1;
+        if (!test.includes(randomNumber)) {
+          test.push(randomNumber);
+          numbers.push(randomNumber);
+        } else if (!test2.includes(randomNumber)) {
+          test2.push(randomNumber);
+          numbers.push(randomNumber);
+        }
       }
-    }
-    cardInput[
-      i
-    ].style.background = `url(./assets/img/${numbers[0]}.jpg) center/cover`;
-    console.log(test);
-  });
-}
+      cardInput[
+        i
+      ].style.background = `url(./assets/img/${numbers[0]}.jpg) center/cover`;
+    });
+  }
+};
