baab41729bfa9f6dc1f60f3e6a6b589c5d421dbe
TheValll <valentin78.massonniere@gmail.com>
2023-10-02 12:48:04 +0200

    mdp generator

--- FromScrach/Data/cours/index.js
+++ FromScrach/Data/cours/index.js
@@ -10,3 +10,23 @@ do {
 
 /// e.target.id
 // setInterval(createLetter, 200);
+
+//// Les méthodes :
+// typeof connaitre le type d'une variable
+// isNan verifie si ce n'est pas un nnombre
+// indexOf permet de trouver la position d'un nombre dans un tableau, retourne -1 si inconnu
+// slice(nunber) coupe une string ou tableau avec le nombre
+// slipt() coupe la valeur donne
+// toLowerCase() met tout en miniscule
+// replace() remplace la valeur  par la valeur 2
+// .toFixed() garde le nombre a virgule a la valeur
+// Math.floor() arrondi au nombre le plus bas
+// Math.ceil() arrondi au nombre le plus haut
+// let newArray = [...arraw1,...arraw2] fussionne é tableaux
+// .join rajoute la valeur entre les valeurs du tableaux
+// .some() true ou false la valeur dans le tableau
+// .reduce() addition les valeur ...
+// .filter() filtrer le tableaux
+// .sort() trier un tableaux
+// .map()
+// navigator.clipboard.writeText(passwordOutput.value);
