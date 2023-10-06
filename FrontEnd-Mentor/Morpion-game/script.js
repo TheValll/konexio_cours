9adb20c688857e03449e68926a248efa0a365dbb
TheValll <valentin78.massonniere@gmail.com>
2023-10-06 22:15:34 +0200

    morpion

--- FrontEnd-Mentor/Morpion-game/script.js
+++ FrontEnd-Mentor/Morpion-game/script.js
@@ -2,18 +2,61 @@ const cells = document.querySelectorAll("[data-cells]");
 const text = document.getElementById("gameText");
 const reload = document.getElementById("reload");
 const playerOne = "X";
-const playerTwo = "0";
+const playerTwo = "O";
 let activePlayer;
 
-const winnerConditions = [0, 1, 2];
-console.log(winnerConditions);
+const winnerConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+const checkDraw = () => {
+  return [...cells].every((cell) => {
+    return cell.innerHTML === playerOne || cell.innerHTML === playerTwo;
+  });
+};
+
+const checkWin = () => {
+  return [...winnerConditions].some((conditions) => {
+    return conditions.every((index) => {
+      return cells[index].innerHTML === activePlayer;
+    });
+  });
+};
+
 const gameLogic = (e) => {
   e.target.innerHTML = activePlayer;
-  activePlayer = activePlayer === playerOne ? playerTwo : playerOne;
-  text.textContent = `Au tour du joueur ${activePlayer} !`;
-
+  e.target.style.color = activePlayer === playerOne ? "green" : "red";
   if (checkDraw()) {
     text.textContent = "Match nul !";
+    cells.forEach((cell) => {
+      cell.removeEventListener("click", gameLogic, { once: true });
+    });
+  } else if (checkWin()) {
+    text.textContent = `Le joueur ${activePlayer} a gagné !`;
+    cells.forEach((cell) => {
+      cell.removeEventListener("click", gameLogic, { once: true });
+    });
+    if (activePlayer === "X") {
+      document.body.style.backgroundColor = "green";
+      cells.forEach((cell) => {
+        cell.style.color = "white";
+      });
+    } else if (activePlayer === "O") {
+      document.body.style.backgroundColor = "red";
+      cells.forEach((cell) => {
+        cell.style.color = "white";
+      });
+    }
+  } else {
+    activePlayer = activePlayer === playerOne ? playerTwo : playerOne;
+    text.textContent = `Au tour du joueur ${activePlayer} !`;
   }
 };
 
@@ -24,20 +67,15 @@ const init = () => {
     cell.innerHTML = "";
   });
   cells.forEach((cell) => {
-    cell.removeEventListener("click", gameLogic);
+    cell.removeEventListener("click", gameLogic, { once: true });
   });
   cells.forEach((cell) => {
-    cell.addEventListener("click", gameLogic);
+    cell.addEventListener("click", gameLogic, { once: true });
   });
+  document.body.style.backgroundColor = "rgb(40, 44, 52)";
 };
 
 init();
-
-const checkDraw = () => {
-  return [...cells].every((cell) => {
-    return cell.innerHTML === playerOne || cell.innerHTML === playerTwo;
-  });
-};
 
 const restart = () => {
   init();
