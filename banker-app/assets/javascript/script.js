b8c3b470ef197303013370aa5417163013c946b5
TheValll <valentin78.massonniere@gmail.com>
2023-10-09 21:44:26 +0200

    bank app date + current balance and login function

--- banker-app/assets/javascript/script.js
+++ banker-app/assets/javascript/script.js
@@ -20,26 +20,63 @@ const data = [
 let userInput = "";
 let pinInput = "";
 
-const afterLogin = document.querySelector(".after--login");
-const welcomeText = document.getElementById("welcome--text");
+const afterLogin = document.querySelector(".app");
+const welcomeText = document.querySelector(".welcome");
+
+//// CREATE LOGIN FUNCTION
 
 const checkLogin = () => {
   for (let i = 0; i < data.length; i++) {
     if (userInput === data[i].user && Number(pinInput) === data[i].pin) {
-      afterLogin.style.display = "block";
+      afterLogin.style.opacity = "1";
       welcomeText.textContent = `Welcome back, ${data[i].firstName} !`;
+      document.querySelector(".login__input--pin").value = "";
+      document.querySelector(".login__input--user").value = "";
     }
   }
 };
 
-document.getElementById("user--input").addEventListener("input", (e) => {
+//// ADD EVENT FOR INPUT USER AND PIN
+
+document.querySelector(".login__input--user").addEventListener("input", (e) => {
   userInput = e.target.value;
 });
 
-document.getElementById("pin--input").addEventListener("input", (e) => {
+document.querySelector(".login__input--pin").addEventListener("input", (e) => {
   pinInput = e.target.value;
 });
 
-document.getElementById("submit--input").addEventListener("click", () => {
+//// FUNCTION LOGIN
+
+document.querySelector(".login__btn").addEventListener("click", (e) => {
+  e.preventDefault();
   checkLogin();
 });
+
+//// CALCUL GLOBAL CURRENT BALANCE
+
+const history = [];
+let currentBalance = document.querySelector(".balance__value");
+const value = document.querySelectorAll(".movements__value");
+value.forEach((e) => {
+  history.push(e.innerHTML);
+});
+let newTable = [];
+for (let i = 0; i < history.length; i++) {
+  newTable += history[i].replace(/\s/g, "").slice(0, -1);
+}
+currentBalance.textContent = `${eval(newTable)} E`;
+
+//// CHANGE DATE LOG
+
+const currentDate = new Date();
+console.log(currentDate);
+const day = currentDate.getDate();
+console.log(day);
+const mounth = currentDate.getMonth() + 1;
+const years = currentDate.getFullYear();
+if (mounth < 10) {
+  mounth = "0" + mounth;
+}
+let dateFormat = mounth + "/" + day + "/" + years;
+document.querySelector(".date").textContent = dateFormat;
