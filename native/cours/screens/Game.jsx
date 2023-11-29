7780e18e6eda9fee0124ca6cee591863b0de58c0
TheValll <valentin78.massonniere@gmail.com>
2023-11-29 01:11:15 +0100

    jump

--- native/cours/screens/Game.jsx
+++ native/cours/screens/Game.jsx
@@ -1,14 +1,24 @@
-import React, { useState } from "react";
+// IMPORT MODULES
+
+import React, { useState, useEffect } from "react";
 import {
   View,
   StyleSheet,
   ImageBackground,
   Image,
+  Text,
   TouchableOpacity,
+  Animated,
 } from "react-native";
 
 const Game = () => {
+  //SET VARIABLES
+
   const [isPressed, setIsPressed] = useState(false);
+  const [bananas, setBananas] = useState([]);
+  const [score, setScore] = useState(0);
+
+  // JUMP FUNCTION
 
   const handlePress = () => {
     setIsPressed(true);
@@ -17,6 +27,37 @@ const Game = () => {
       setIsPressed(false);
     }, 300);
   };
+
+  // BANANA ANIMATION
+
+  useEffect(() => {
+    const bananaAnimation = new Animated.Value(-50);
+    const bananaInterval = setInterval(() => {
+      setBananas((prevBananas) => {
+        return [...prevBananas, bananaAnimation];
+      });
+
+      Animated.timing(bananaAnimation, {
+        toValue: 450,
+        duration: 2500,
+        useNativeDriver: true,
+      }).start(() => {
+        const index = bananas.indexOf(bananaAnimation);
+        if (index !== -1) {
+          setBananas((prevBananas) => {
+            const updatedBananas = [...prevBananas];
+            updatedBananas.splice(index, 1);
+            return updatedBananas;
+          });
+          bananaAnimation.setValue(-50);
+        }
+      });
+    }, 2000);
+
+    return () => {
+      clearInterval(bananaInterval);
+    };
+  }, [bananas]);
 
   return (
     <ImageBackground
@@ -27,21 +68,24 @@ const Game = () => {
       <View style={styles.heartbar}>
         <Image source={require("../assets/3hp.png")} />
       </View>
-      <Image
-        source={require("../assets/banana.png")}
-        style={[styles.banana, { width: 50, height: 50 }]}
-      />
-      <Image
-        source={require("../assets/croco.png")}
-        style={[styles.croco, { width: 175, height: 100 }]}
-      />
+      <Text style={styles.score}>Score : {score}</Text>
+      {bananas.map((banana, index) => (
+        <Animated.Image
+          key={index}
+          source={require("../assets/banana.png")}
+          style={[
+            styles.banana,
+            { width: 50, height: 50, transform: [{ translateX: banana }] },
+          ]}
+        />
+      ))}
       <View style={styles.character}>
         <TouchableOpacity
           onPress={handlePress}
           activeOpacity={1}
           style={[
             styles.monkey,
-            isPressed ? { transform: [{ translateY: -300 }] } : null,
+            isPressed ? { transform: [{ translateY: -325 }] } : null,
           ]}
         >
           <Image source={require("../assets/monkey.png")} />
@@ -59,13 +103,17 @@ const styles = StyleSheet.create({
     position: "absolute",
     margin: 20,
   },
+  score: {
+    position: "absolute",
+    color: "white",
+    top: "30%",
+    left: "35%",
+    fontSize: 36,
+  },
   banana: {
     position: "absolute",
-    top: 450,
-  },
-  croco: {
-    position: "absolute",
-    bottom: 0,
+    top: "50%",
+    left: -50,
   },
   character: {
     minHeight: "100%",
