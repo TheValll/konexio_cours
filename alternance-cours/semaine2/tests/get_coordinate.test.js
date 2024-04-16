64340761ab4edd58044b2e5be4bc64f76462ae5f
Valentin Massonnière <vmassonniere@adloox.com>
2024-04-16 16:47:08 +0200

    added change

--- alternance-cours/semaine2/tests/get_coordinate.test.js
+++ alternance-cours/semaine2/tests/get_coordinate.test.js
@@ -5,6 +5,11 @@ describe("get_coordinates", () => {
     const address = "1600 Amphitheatre Parkway, Mountain View, CA";
     const coordinates = await get_coordinates(address);
     expect(coordinates).toEqual([37.422408, -122.085608]);
+    if (coordinates[0] === 37.422408 && coordinates[1] === -122.085608) {
+      console.log("Test passed");
+    } else {
+      console.log("Test failed");
+    }
   });
 
   test("throws an error for an invalid address", async () => {
