c58cc423cedd03f6d5e1f6c9be0c9614df407b92
Valentin Massonnière <vmassonniere@adloox.com>
2024-04-16 11:36:39 +0200

    change api

--- alternance-cours/semaine2/app/app.py
+++ alternance-cours/semaine2/app/app.py
@@ -15,7 +15,7 @@ from geopy.geocoders import Nominatim
 def main():
     st.set_page_config(
         page_title="Entreprise Distance Calulateur", 
-        layout="centered", 
+        layout="wide", 
         initial_sidebar_state="auto",
     )
 if __name__ == "__main__":
@@ -37,16 +37,18 @@ st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 # Function to get the latitude and longitude of an address
 def get_latitude_longitude(address):
-    loc = Nominatim(user_agent="Geopy Library")
-    location = loc.geocode(address, timeout=10)
+    api_url = f"https://api-adresse.data.gouv.fr/search/?q={address}&type=street"
+    response = requests.get(api_url)
+    api_data = response.json()
+    location = api_data['features'][0]['geometry']['coordinates']
     if location:
         return location
     else:
         return None
 
 # Function to get the distance between two coordinates
-def get_distance(latitude1, longitude1, latitude2, longitude2):
-    api_url = f"https://api.distancematrix.ai/maps/api/distancematrix/json?origins={latitude1},{longitude1}&destinations={latitude2},{longitude2}&key={os.getenv('ACCES_API_KEY')}"
+def get_distance(latitude1, longitude1, latitude2, longitude2, mode='car'):
+    api_url = f"https://wxs.ign.fr/calcul/geoportail/itineraire/rest/1.0.0/route?resource=bdtopo-pgr&profile={mode}&optimization=fastest&start={longitude1},{latitude1}&end={longitude2},{latitude2}"
     response = requests.get(api_url)
     api_data = response.json()
     return api_data
@@ -57,13 +59,12 @@ def convert_csv_to_json(uploaded_file):
     json_data = data.to_json(orient='records')
     try:
         json_obj = json.loads(json_data)
-        with open('data.json', 'w') as f:
-            json.dump(json_obj, f)
         return json_obj
     except Exception as e:
         st.write(f"Erreur lors de l'écriture du fichier : {e}")
 
 # ------------------------------------------------
+
 
 # Call the Nominatim API
 loc = Nominatim(user_agent="Geopy Library")
@@ -77,8 +78,11 @@ entreprise_adresses = st.text_input("Entrez l'adresse de l'entreprise", placehol
 # Create a submit button
 nom = []
 prenom = []
-distances = []
-temps = []
+distances_voiture = []
+temps_voiture = []
+distances_pieton = []
+temps_pieton = []
+temps_velo = []
 if st.button('Calculer la distance'):
     if uploaded_files:
         data = convert_csv_to_json(uploaded_files)
@@ -91,25 +95,36 @@ if st.button('Calculer la distance'):
                 adresse = f"{people['Adresse']}, {people['Code postal']}, {people['Ville']}"
                 coordinate = get_latitude_longitude(adresse)
                 if coordinate and entreprise_coordinate:
-                    distance = get_distance(entreprise_coordinate.latitude, entreprise_coordinate.longitude, coordinate.latitude, coordinate.longitude)
-                    if distance:
+                    distance_voiture = get_distance(entreprise_coordinate[1], entreprise_coordinate[0], coordinate[1], coordinate[0], mode='car')
+                    distance_pieton = get_distance(entreprise_coordinate[1], entreprise_coordinate[0], coordinate[1], coordinate[0], mode='pedestrian')
+                    if distance_voiture:
                         nom.append(people['Nom'])
                         prenom.append(people['Prénom'])
-                        distances.append(distance['rows'][0]['elements'][0]['distance']['text'])
-                        temps.append(distance['rows'][0]['elements'][0]['duration']['text'])
+                        distances_voiture.append(round(distance_voiture['distance']/1000, 2))
+                        temps_voiture.append(round(distance_voiture['duration']/60, 2))
+                    if distance_pieton:
+                        distances_pieton.append(round(distance_voiture['distance']/1000, 2))
+                        temps_pieton.append(round(distance_pieton['duration']/60, 2))
+                        temps_velo.append(round((distance_pieton['duration']/60)/3, 2))
                 else:
                     nom.append(people['Nom'])
                     prenom.append(people['Prénom'])
-                    distances.append("None")
-                    temps.append("None")
+                    distances_voiture.append("None")
+                    temps_voiture.append("None")
+                    distances_pieton.append("None")
+                    temps_pieton.append("None")
+                    temps_velo.append("None")
     else:
         st.write("No file uploaded")
 
-if nom and prenom and distances and temps:
+if nom and prenom and distances_voiture and temps_voiture and distances_pieton and temps_pieton:
     df = pd.DataFrame({
         'Nom': nom,
         'Prénom': prenom,
-        'Distance': distances,
-        'Temps': temps
+        'Distance voiture en km': distances_voiture,
+        'Temps voiture en minutes': temps_voiture,
+        'Distance piéton en km': distances_pieton,
+        'Temps piéton en minutes': temps_pieton,
+        'Temps vélo en minutes': temps_velo
     })
     st.write(df)
